1ec9d8b134341269d8c889a67827486c6a82c5c7
t-sin <shinichi.tanaka45@gmail.com>
2020-12-11 10:32:41 +0900

    Parse MML-like language

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+////
 // Utilities
 
 // Frequency that cossresponds to concert A.
@@ -15,18 +16,99 @@ const convert_to_frequency = (notenum) => {
   return freq;
 };
 
+// syntactic elements
+const syntax_note = /^([cdefgab])([0-9])([+-]?):([0-9]+)$/;
+const syntax_rest = /^([r=]):([0-9]+)$/;
+const syntax_track = /^---$/;
+const syntax_param = /^([^@=]+)=(-?[0-9]+(.[0-9]+))$/;
+
+const syntax_type = (token) => {
+  if (syntax_note.test(token)) {
+    return 'note';
+  } else if (syntax_rest.test(token)) {
+    return 'rest';
+  } else if (syntax_track.test(token)) {
+    return 'track';
+  } else if (syntax_param.test(token)) {
+    return 'param';
+  } else {
+    return undefined;
+  }
+};
+
+////
 // MML Player API
 
-// Parse `str` and return track information.
-const parse = (str) => {};
+// Parse `str` and return an AST list.
+const parse = (str) => {
+  let tokens = str.split(/[ \n]+/);
+  let ast_list = [];
+
+  for (token of tokens) {
+    let node = null;
+    let match = null;
+
+    switch (syntax_type(token)) {
+      case 'note':
+        match = token.match(syntax_note);
+        node = {
+          'type': 'note',
+          'note_name': match[1],
+          'octave': Number.parseFloat(match[2]),
+          'half_note': match[3],
+          'length': Number.parseFloat(match[4]),
+        };
+        break;
+
+      case 'rest':
+        match = token.match(syntax_rest);
+        node = {
+          'type': 'rest',
+          'note_name': match[1],
+          'length': Number.parseFloat(match[2]),
+        }
+        break;
+
+      case 'track':
+        match = token.match(syntax_rest);
+        node = {
+          'type': 'track',
+        }
+        break;
+
+      case 'param':
+        match = token.match(syntax_rest);
+        node = {
+          'type': 'param',
+          'name': match[1],
+          'value': Number.parseFloat(match[2]),
+        }
+        break;
+
+      default:
+        throw Error(`unknown token '$token'`);
+    }
+
+    ast_list.push(node);
+  }
+
+  console.log(ast_list);
+  return ast_list;
+};
+
 // Create player object which has scheduled notes and events.
 const make_player = (ast) => {};
+
 // Start playing.
 const play = (player) => {};
+
 // Stop playing.
 const stop = (player) => {};
 
+
+////
 // For HTML interface
+
 let player = null;
 const play_button = () => {
   let mml = document.getElementById('mmlcode').value;
