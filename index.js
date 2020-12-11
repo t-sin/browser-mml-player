54881905f0789298571bf0dba69e4c6f803f90a2
t-sin <shinichi.tanaka45@gmail.com>
2020-12-11 09:49:53 +0900

    Basec structure

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+// Utilities
+
 // Frequency that cossresponds to concert A.
 const concert_a_freq = 440;
 // MIDI note number that denotes concert A.
@@ -12,3 +14,30 @@ const convert_to_frequency = (notenum) => {
   const freq = Math.pow(2, from_concert_a / 12) * concert_a_freq;
   return freq;
 };
+
+// MML Player API
+
+// Parse `str` and return track information.
+const parse = (str) => {};
+// Create player object which has scheduled notes and events.
+const make_player = (ast) => {};
+// Start playing.
+const play = (player) => {};
+// Stop playing.
+const stop = (player) => {};
+
+// For HTML interface
+let player = null;
+const play_button = () => {
+  let mml = document.getElementById('mmlcode').value;
+  player = make_player(parse(mml));
+  play(player);
+}
+
+const stop_button = () => {
+  stop(player);
+}
+
+// Configure event handlers
+document.getElementById('mmlplay').addEventListener('click', play_button);
+document.getElementById('mmlstop').addEventListener('click', stop_button);
